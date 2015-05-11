4388e7faa209263f514b970a972417b4698cefad
Chris Kelly <ckelly@newsinc.com>
2015-05-11 16:51:24 -0400

    yml to yaml since thats what the gods demand

--- lib/vominator.rb
+++ lib/vominator.rb
@@ -3,13 +3,13 @@ require "vominator/version"
 
 module Vominator
   def self.get_config
-    config_file = File.expand_path("~/.vominator.yml")
+    config_file = File.expand_path("~/.vominator.yaml")
     vominator_config = YAML.load(File.read(config_file))
     return vominator_config if vominator_config.kind_of?(Hash)
   end
 
   def self.get_puke_config(puke_dir)
-    config_file = "#{puke_dir}/config.yml"
+    config_file = "#{puke_dir}/config.yaml"
     puke_config = YAML.load(File.read(config_file))
     return puke_config if puke_config.kind_of?(Hash)
   end
