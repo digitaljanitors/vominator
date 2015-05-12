b11489750136b2b8386b9d3e43c6aefb1bedafb5
Chris Kelly <ckelly@newsinc.com>
2015-05-12 14:52:21 -0400

    better location for previous commit

--- lib/vominator/ec2.rb
+++ lib/vominator/ec2.rb
@@ -1,7 +1,12 @@
+require './constants.rb'
 module Vominator
   class EC2
-    def self.helloworld(name)
-      puts "Hello #{name}!"
+    def self.get_virt_type(instance_type)
+      return EC2_INSTANCE_METADATA[instance_type.to_sym][:virtualization_type]
+    end
+
+    def self.get_ephemeral_dev_count(instance_type)
+      return EC2_INSTANCE_METADATA[instance_type.to_sym][:ephemeral_devices]
     end
   end
 end
