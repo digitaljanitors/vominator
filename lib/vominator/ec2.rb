86baadd359cf76183ee31e454b757e50d88f4f03
Chris Kelly <ckelly@newsinc.com>
2015-05-18 01:00:23 -0400

    ec2 helpers

--- lib/vominator/ec2.rb
+++ lib/vominator/ec2.rb
@@ -1,4 +1,6 @@
+require 'aws-sdk'
 require './constants.rb'
+
 module Vominator
   class EC2
     def self.get_virt_type(instance_type)
@@ -7,6 +9,18 @@ module Vominator
 
     def self.get_ephemeral_dev_count(instance_type)
       return EC2_INSTANCE_METADATA[instance_type.to_sym][:ephemeral_devices]
+    end
+
+    def self.get_instances(resource)
+      resource.instances
+    end
+
+    def self.get_security_groups(resource)
+
+    end
+
+    def self.get_subnets(resource)
+
     end
   end
 end
