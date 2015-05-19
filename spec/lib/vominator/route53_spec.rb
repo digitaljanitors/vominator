a1ec81942a40f36f79029849dfd2f907efa7c81f
Chris Kelly <ckelly@newsinc.com>
2015-05-19 16:45:39 -0400

    stub in is_truncated true and false randomziation on route53 list_resource_record_sets

--- spec/lib/vominator/route53_spec.rb
+++ spec/lib/vominator/route53_spec.rb
@@ -9,7 +9,10 @@ describe Vominator::Route53 do
                                                       { name:'sample-api-1.test.example.com.', resource_records:[{ value: '10.203.41.21'}] },
                                                       { name:'sample-api-2.test.example.com.', resource_records:[{ value: '10.203.42.21'}] },
                                                       { name:'sample-api-3.test.example.com.', resource_records:[{ value: '10.203.43.21'}] }
-                                                  ])
+                                                  ],
+                                                  is_truncated: true,
+                                                  is_truncated: false
+    )
   end
 
   describe 'get_records' do
