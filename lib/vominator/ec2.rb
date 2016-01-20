4daa893a60cf297287f58ee1e845b306ce014dfa
Chris McNabb <raizyr@gmail.com>
2016-01-20 10:28:24 -0600

    more tests

--- lib/vominator/ec2.rb
+++ lib/vominator/ec2.rb
@@ -5,7 +5,11 @@ require_relative 'constants'
 module Vominator
   class EC2
     def self.get_virt_type(instance_type)
-      return EC2_INSTANCE_METADATA[instance_type.to_sym][:virtualization_type]
+      begin
+        return EC2_INSTANCE_METADATA[instance_type.to_sym][:virtualization_type]
+      rescue NoMethodError
+        raise ArgumentError, 'You must specify a valid instance type'
+      end
     end
 
     def self.get_ephemeral_dev_count(instance_type)
